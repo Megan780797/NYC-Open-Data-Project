d1bf71de699f05ebbc953fb133ea0da7da3b53c2
Megan780797 <MEganc86@nycstudents.net>
2026-06-01 17:18:34 +0000

    ss

--- script.js
+++ script.js
@@ -7,6 +7,22 @@ async function init(){
   data = await info.json();
   console.log(data); 
 }
+function FireCausesByBorough(){
+  //Variables to keep count of accidents by borough
+  let Queens= 0, Manhattan = 0, Brooklyn=0, Bronx=0;
+}
+for(let i = 0; i < data.length; i++){
+    let fire = data[i];
+    if(Fire.borough == "QUEENS"){
+      Queens++;
+    }else if(Fire.borough == "MANHATTAN"){
+      Manhattan++;
+    }else if(Fire.borough == "BROOKLYN"){
+      Brooklyn++;
+    }else if(Fire.borough == "BRONX"){
+      Bronx++;
+  }
+}
  let output = document.getElementById("output");
   let build = "";
   for(let i = 0; i < data.length; i+=1){
@@ -16,11 +32,13 @@ async function init(){
       <h1${fire.case_year}</h1>
     <h3>${fire.cause_fire_description}</h3>
     <p>${fire.borough}</p>
+ output.innerHTML = build;
      </div>`   
   }
-  output.innerHTML = build;
-
-function FireCausesByBorough(){
-  //Variables to keep count of accidents by borough
-  let Queens= 0, Manhattan = 0, Brooklyn=0, Bronx=0;
-}
+    //Creating data for chart (as array of arrays) with 1st position of array being label
+  let chartData = [
+    ["QUEENS",Queens],
+    ["MANHATTAN", Manhattan],
+    ["BROOKLYN",Brooklyn ],
+    ["BRONX", Bronx],
+  ];
