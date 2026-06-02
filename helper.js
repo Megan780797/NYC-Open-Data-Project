a113e309938fb4f5518760d5e179c31ce7d960f6
Megan780797 <MEganc86@nycstudents.net>
2026-06-02 17:22:15 +0000

    ss

---
+++ helper.js
@@ -0,0 +1,28 @@
+// get() returns the element using document.getElementById().
+function get(id){
+  return document.getElementById(id);
+}
+
+// Create the function card() to generate an appropriate info card
+function card( info ){ 
+    let build = `<div class="card fitted">
+                    <h1${info.case_year}</h1>
+                    <h3>${info.cause_fire_description}</h3>
+                    <p>${info.borough}</p>
+                </div>`;
+
+    return build;
+}
+
+
+
+
+function displayChart( data, chart_id, chart_type ){
+  c3.generate({
+    bindto: `#${chart_id}`, // id of the div to display chart
+    data: {
+      columns: data, // data must be an array of arrays
+      type: chart_type // type of chart (pie/line/bar)
+    }
+  });
+}
--- script.js
+++ script.js
@@ -4,41 +4,49 @@ let data, info, output;
 
 async function init(){
   let link = "FireCauses.json"; //https://data.cityofnewyork.us/Public-Safety/Bureau-of-Fire-Investigations-Fire-Causes/ii3r-svjz/about_data"
+  info = await fetch(link);
   data = await info.json();
   console.log(data); 
+
+  output = get("output");
+  let build = "";
+
+  //Build info cards with button to show map if lat and lon values exist
+  for(let i = 0; i < data.length; i++){
+    let fire = data[i];
+      build += card(fire);    
+  }
+
+  //Display cards in the div with id "leftPanel"
+   output.innerHTML = build;
+
 }
+
 function FireCausesByBorough(){
   //Variables to keep count of accidents by borough
   let Queens= 0, Manhattan = 0, Brooklyn=0, Bronx=0;
-}
-for(let i = 0; i < data.length; i++){
-    let fire = data[i];
-    if(Fire.borough == "QUEENS"){
-      Queens++;
-    }else if(Fire.borough == "MANHATTAN"){
-      Manhattan++;
-    }else if(Fire.borough == "BROOKLYN"){
-      Brooklyn++;
-    }else if(Fire.borough == "BRONX"){
-      Bronx++;
+  for(let i = 0; i < data.length; i++){
+      let fire = data[i];
+      if(fire.borough == "QUEENS"){
+        Queens++;
+      }else if(fire.borough == "MANHATTAN"){
+        Manhattan++;
+      }else if(fire.borough == "BROOKLYN"){
+        Brooklyn++;
+      }else if(fire.borough == "BRONX"){
+        Bronx++;
+      }
   }
+  // Need to add other code for creating Charts
 }
- let output = document.getElementById("output");
-  let build = "";
-  for(let i = 0; i < data.length; i+=1){
-    let fire = data[i];
-      build+=`<div class="card">
-      <hr> 
-      <h1${fire.case_year}</h1>
-    <h3>${fire.cause_fire_description}</h3>
-    <p>${fire.borough}</p>
- output.innerHTML = build;
-     </div>`   
-  }
+
+
     //Creating data for chart (as array of arrays) with 1st position of array being label
-  let chartData = [
-    ["QUEENS",Queens],
-    ["MANHATTAN", Manhattan],
-    ["BROOKLYN",Brooklyn ],
-    ["BRONX", Bronx],
-  ];
+  // let chartData = [
+  //   ["QUEENS",Queens],
+  //   ["MANHATTAN", Manhattan],
+  //   ["BROOKLYN",Brooklyn ],
+  //   ["BRONX", Bronx],
+  // ];
+
+
