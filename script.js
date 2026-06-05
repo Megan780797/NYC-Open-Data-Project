92740a51999989d0c62c06d8f9254eaa53f61577
Megan780797 <MEganc86@nycstudents.net>
2026-06-05 16:03:09 +0000

    ss

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ async function init(){
 
   output = get("output");
   let build = "";
-
   //Build info cards with button to show map if lat and lon values exist
   for(let i = 0; i < data.length; i++){
     let fire = data[i];
@@ -24,29 +23,33 @@ async function init(){
 
 function FireCausesByBorough(){
   //Variables to keep count of accidents by borough
-  let Queens= 0, Manhattan = 0, Brooklyn=0, Bronx=0;
   for(let i = 0; i < data.length; i++){
+    let Queens= 0, Manhattan = 0,  Staten=0, Brooklyn=0, Bronx=0;
       let fire = data[i];
       if(fire.borough == "QUEENS"){
         Queens++;
-      }else if(fire.borough == "MANHATTAN"){
+      } else if(fire.borough == "MANHATTAN"){
         Manhattan++;
-      }else if(fire.borough == "BROOKLYN"){
-        Brooklyn++;
-      }else if(fire.borough == "BRONX"){
-        Bronx++;
+      } else if(fire.borough == "BROOKLYN"){
+        brooklyn++;
+      } else if(fire.borough == "BRONX"){
+        bronx++;
       }
+        else if(fire.borough == "STATEN ISLAND"){
+          Staten++;
+        }
+        
+      
   }
   // Need to add other code for creating Charts
 }
 
 
     //Creating data for chart (as array of arrays) with 1st position of array being label
-  // let chartData = [
-  //   ["QUEENS",Queens],
-  //   ["MANHATTAN", Manhattan],
-  //   ["BROOKLYN",Brooklyn ],
-  //   ["BRONX", Bronx],
-  // ];
-
-
+  let chartData = [
+    ["QUEENS",queens],
+    ["MANHATTAN",manhattan],
+    ["BROOKLYN", brooklyn],
+    ["BRONX", bronx],
+    ["STATEN ISLAND", Staten]
+  ];
